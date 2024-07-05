571ac5fc8b173d14156baec130e6a8979dad68dc
MICHAL FERNANDEZ <michaeltkd14@gmail.com>
2024-07-05 11:42:26 -0600

    "delate spinner"

--- src/App.tsx
+++ src/App.tsx
@@ -4,41 +4,17 @@ import About from "./components/About"
 import Services from "./components/Services"
 import Projects from "./components/Projects"
 import Footer from "./components/Footer"
-import { useEffect, useState } from "react"
-import {Circles } from "react-loader-spinner"
 import Contact from "./components/Contact"
 
 const App=()=> {
  
-  const[loading,setLoading]=useState(false)
-  useEffect(()=>{
-    setLoading(true)
-    setTimeout(()=>{
-      setLoading(false)
-},1000)
 
- },[])
   return (
 
    
       
     <>
-    {
-      loading ?
-    <div className="bg-blue-600 h-100 flex justify-center items-center">
-      <Circles
-      height="80"
-      width="80"
-      color="#d946ef"
-      ariaLabel="circles-loading"
-      wrapperStyle={{}}
-      wrapperClass=""
-      visible={true}
-      /> 
-     </div>
-      
 
-      :
       <div className="bg-gradient-to-r from-blue-600 to-blue-900">
 
       <Navbar/>
@@ -49,7 +25,7 @@ const App=()=> {
       <Contact/>
       <Footer/>
       </div>
-    }
+    
     </>
      
 
